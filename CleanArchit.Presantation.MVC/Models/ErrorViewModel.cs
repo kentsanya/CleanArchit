7748f964d88bf968d1c3db6e301dd0ee2576f1b2
Oleksandr Vdovitchenko <kentsanya91@gmail.com>
2023-02-20 15:20:34 +0200

    Initial structure Clean Architecture

---
+++ CleanArchit.Presantation.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArchit.Presantation.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
