02b95dafc00d89221e5930ce13e130d8594ac802
Oleksandr Vdovitchenko <kentsanya91@gmail.com>
2023-03-06 15:10:03 +0200

    Adding Domain.Core and implemetation in Infrastructure.Bus

---
+++ Infrastructure.Bus/InMemoryBus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchit.Domain.Core.Bus;
+using MediatR;
+
+namespace CleanArchit.Infrastructure.Bus
+{
+    public class InMemoryBus:IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator) 
+        {
+            _mediator = mediator;
+        }
+        public Task SendCommand<T>(T command) where T:Command 
+        {
+            return _mediator.Send(command);
+        }
+
+    }
+}
